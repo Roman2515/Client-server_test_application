b21d5ed31c4f1f41d3095f41752efe2e7b38b0a2
Roman <rmuliavin@mail.ru>
2016-04-28 21:34:21 +0600

    First

---
+++ ConsoleApplication1/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SocketServer
+{
+    public class User
+    {
+        public User()
+        {
+            Id = Guid.NewGuid();
+            user = "Margaret";
+            Dt = DateTime.Now;
+            value = rand.Next(-100, 101) / 10f;
+        }
+        public Guid Id { get; set; }
+        public string user { get; set; }
+        public DateTime Dt { get; set; }
+        public float value { get; set; }
+        private Random rand = new Random();
+        public void Print()
+        {
+            Console.WriteLine("Guid: {0}\nUser: {1}\nDateTime: {2}\nValue: {3}\n", Id, user, Dt, value);
+        }
+    }
+}
---
+++ ConsoleApplication1/UserContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace SocketServer
+
+{
+    class UserContext : DbContext
+    {
+        public UserContext(string conectionString)
+            : base(conectionString)
+        { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
